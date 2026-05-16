98d5ae6d23cb19f9ed70e154c245c74a6437d70a
root <root@srv1610933.hstgr.cloud>
2026-05-16 05:37:33 +0000

    Center combat layout for 1080p scene

--- combat/combat_scene.gd
+++ combat/combat_scene.gd
@@ -12,11 +12,11 @@ const CARD_VIEW_SCENE = preload("res://cards/card_view.tscn")
 @onready var gold_label = $RootLayout/TopBar/GoldLabel
 @onready var status_label = $RootLayout/TopBar/StatusLabel
 
-@onready var board_card_list = $RootLayout/PlayArea/BoardSection/BoardCardList
+@onready var board_card_list = $RootLayout/PlayArea/BoardCenter/BoardSection/BoardCardList
 
-@onready var left_hand_label = $RootLayout/PlayArea/LabelRow/LeftHandLabel
-@onready var right_hand_label = $RootLayout/PlayArea/LabelRow/RightHandLabel
-@onready var backpack_label = $RootLayout/PlayArea/LabelRow/BackpackLabel
+@onready var left_hand_label = $RootLayout/PlayArea/LoadoutCenter/LoadoutGroup/LabelRow/LeftHandLabel
+@onready var right_hand_label = $RootLayout/PlayArea/LoadoutCenter/LoadoutGroup/LabelRow/RightHandLabel
+@onready var backpack_label = $RootLayout/PlayArea/LoadoutCenter/LoadoutGroup/LabelRow/BackpackLabel
 @onready var take_first_monster_button = $RootLayout/ButtonBar/TakeFirstMonsterButton
 @onready var move_first_to_left_hand_button = $RootLayout/ButtonBar/MoveFirstToLeftHandButton
 @onready var move_first_to_backpack_button = $RootLayout/ButtonBar/MoveFirstToBackpackButton
